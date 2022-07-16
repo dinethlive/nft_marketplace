af2d2f447d524f015b5db0d4aff225935b08b4f5
dinethlive <dinethlive@gmail.com>
2022-07-16 16:22:21 +0530

    Add Navbar & Footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
+import { Footer, Navbar } from '../components';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const Marketplace = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <div className="pt-65">
+        <Component {...pageProps} />
+      </div>
+      <Footer />
+    </div>
 
-export default MyApp
+    <Script src="https://kit.fontawesome.com/b6f2a4e7de.js" crossOrigin="anonymous" />
+  </ThemeProvider>
+);
+
+export default Marketplace;
