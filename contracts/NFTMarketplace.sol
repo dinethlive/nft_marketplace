4d39654066969804a6b6e18250c649f3e87b1c77
dinethlive <dinethlive@gmail.com>
2022-07-17 02:39:29 +0530

    Add resellToken function

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -80,4 +80,17 @@ contract NFTMarketplace is ERC721URIStorage {
       );
     }
 
+    /* allows someone to resell a token they have purchased */
+    function resellToken(uint256 tokenId, uint256 price) public payable {
+      require(idToMarketItem[tokenId].owner == msg.sender, "Only item owner can perform this operation");
+      require(msg.value == listingPrice, "Price must be equal to listing price");
+      idToMarketItem[tokenId].sold = false;
+      idToMarketItem[tokenId].price = price;
+      idToMarketItem[tokenId].seller = payable(msg.sender);
+      idToMarketItem[tokenId].owner = payable(address(this));
+      _itemsSold.decrement();
+
+      _transfer(msg.sender, address(this), tokenId);
+    }
+
 }
