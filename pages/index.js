38147ea140078d374846de84673298db51cdd357
dinethlive <dinethlive@gmail.com>
2022-07-16 15:38:23 +0530

    Project Setup

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+// eslint-disable-next-line react/function-component-definition
+export default function Home() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
+}
