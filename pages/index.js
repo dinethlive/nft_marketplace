2fc17669a789151a2abba6e313310becdca92647
dinethlive <dinethlive@gmail.com>
2022-07-16 18:45:27 +0530

    Fix Index.js

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,6 @@ import Image from 'next/image';
 // eslint-disable-next-line react/function-component-definition
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <></>
   );
 }
