337381339c4ac065f46b5e0a5749bbf39038c415
dinethlive <dinethlive@gmail.com>
2022-07-17 15:55:38 +0530

    fix SearchBar

--- components/index.js
+++ components/index.js
@@ -6,5 +6,6 @@ import Input from './Input';
 import NFTCard from './NFTCard';
 import CreatorCard from './CreatorCard';
 import Loader from './Loader';
+import SearchBar from './SearchBar';
 
-export { Navbar, Footer, Button, Banner, Input, NFTCard, CreatorCard, Loader };
+export { Navbar, Footer, Button, Banner, Input, NFTCard, CreatorCard, Loader, SearchBar };
