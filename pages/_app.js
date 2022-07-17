991715c817778f306d108a5321833c84d1d94ee3
dinethlive <dinethlive@gmail.com>
2022-07-17 09:57:22 +0530

    Create NFTContext

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,23 @@
 import Script from 'next/script';
 import { ThemeProvider } from 'next-themes';
+import { NFTProvider } from '../context/NFTContext';
 import { Footer, Navbar } from '../components/index';
 import '../styles/globals.css';
 
 const Marketplace = ({ Component, pageProps }) => (
-  <ThemeProvider attribute="class">
-    <div className="dark:bg-nft-dark bg-white min-h-screen">
-      <Navbar />
-      <div className="pt-65">
-        <Component {...pageProps} />
+  <NFTProvider>
+    <ThemeProvider attribute="class">
+      <div className="dark:bg-nft-dark bg-white min-h-screen">
+        <Navbar />
+        <div className="pt-65">
+          <Component {...pageProps} />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
 
-    <Script src="https://kit.fontawesome.com/b6f2a4e7de.js" crossOrigin="anonymous" />
-  </ThemeProvider>
+      <Script src="https://kit.fontawesome.com/b6f2a4e7de.js" crossOrigin="anonymous" />
+    </ThemeProvider>
+  </NFTProvider>
 );
 
 export default Marketplace;
