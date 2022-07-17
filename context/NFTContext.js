d7aa5415fad8c4e9d5778e7040484ba9a0e3fc73
dinethlive <dinethlive@gmail.com>
2022-07-17 15:14:37 +0530

    Add buyNft function

--- context/NFTContext.js
+++ context/NFTContext.js
@@ -50,6 +50,20 @@ export const NFTProvider = ({ children }) => {
     await transaction.wait();
   };
 
+  const buyNft = async (nft) => {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(MarketAddress, MarketAddressABI, signer);
+
+    const price = ethers.utils.parseUnits(nft.price.toString(), 'ether');
+    const transaction = await contract.createMarketSale(nft.tokenId, { value: price });
+    setIsLoadingNFT(true);
+    await transaction.wait();
+    setIsLoadingNFT(false);
+  };
+
   const connectWallet = async () => {
     if (!window.ethereum) return alert('Please install MetaMask.');
 
