97ead1602be85a612c89a69592a8f6f3f2f8b893
dinethlive <dinethlive@gmail.com>
2022-07-17 12:00:04 +0530

    create connectWallet function

--- context/NFTContext.js
+++ context/NFTContext.js
@@ -8,9 +8,21 @@ import { MarketAddress, MarketAddressABI } from './constants';
 export const NFTContext = React.createContext();
 export const NFTProvider = ({ children }) => {
   const nftCurrency = 'ETH';
+  const [currentAccount, setCurrentAccount] = useState('');
+  const connectWallet = async () => {
+    if (!window.ethereum) return alert('Please install MetaMask.');
 
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+    setCurrentAccount(accounts[0]);
+    window.location.reload();
+  };
+
+  useEffect(() => {
+    // checkIfWalletIsConnect();
+  }, []);
   return (
-    <NFTContext.Provider value={{ nftCurrency }}>
+    <NFTContext.Provider value={{ nftCurrency, connectWallet, currentAccount }}>
       {children}
     </NFTContext.Provider>
   );
