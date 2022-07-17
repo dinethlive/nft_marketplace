9c25bb5cdfb77f62243570de9ba5010c9fe54871
dinethlive <dinethlive@gmail.com>
2022-07-17 11:00:04 +0530

    add utils

--- components/index.js
+++ components/index.js
@@ -4,5 +4,6 @@ import Button from './Button';
 import Banner from './Banner';
 import Input from './Input';
 import NFTCard from './NFTCard';
+import CreatorCard from './CreatorCard';
 
-export { Navbar, Footer, Button, Banner, Input, NFTCard };
+export { Navbar, Footer, Button, Banner, Input, NFTCard, CreatorCard };
