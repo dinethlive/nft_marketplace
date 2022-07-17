e1a6cd28ea9e81c747f6151ab597fb9a561d62db
dinethlive <dinethlive@gmail.com>
2022-07-17 08:15:14 +0530

    Add fetchMyNFTs function

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -108,7 +108,7 @@ contract NFTMarketplace is ERC721URIStorage {
       payable(owner).transfer(listingPrice);
       payable(idToMarketItem[tokenId].seller).transfer(msg.value);
     }
-    
+
     /* Returns all unsold market items */
     function fetchMarketItems() public view returns (MarketItem[] memory) {
       uint itemCount = _tokenIds.current();
@@ -126,5 +126,29 @@ contract NFTMarketplace is ERC721URIStorage {
       }
       return items;
     }
+    /* Returns only items that a user has purchased */
+    function fetchMyNFTs() public view returns (MarketItem[] memory) {
+      uint totalItemCount = _tokenIds.current();
+      uint itemCount = 0;
+      uint currentIndex = 0;
+
+      for (uint i = 0; i < totalItemCount; i++) {
+        if (idToMarketItem[i + 1].owner == msg.sender) {
+          itemCount += 1;
+        }
+      }
+
+      MarketItem[] memory items = new MarketItem[](itemCount);
+      for (uint i = 0; i < totalItemCount; i++) {
+        if (idToMarketItem[i + 1].owner == msg.sender) {
+          uint currentId = i + 1;
+          MarketItem storage currentItem = idToMarketItem[currentId];
+          items[currentIndex] = currentItem;
+          currentIndex += 1;
+        }
+      }
+      return items;
+    }
+
 
 }
