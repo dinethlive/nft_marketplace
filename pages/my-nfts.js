2d686682da394e13b6819353a5b5ff5d49dafab7
dinethlive <dinethlive@gmail.com>
2022-07-18 17:55:24 +0530

    fix sorting my-nfts

--- pages/my-nfts.js
+++ pages/my-nfts.js
@@ -20,7 +20,39 @@ const MyNFTs = () => {
         setIsLoading(false);
       });
   }, []);
+  useEffect(() => {
+    const sortedNfts = [...nfts];
 
+    switch (activeSelect) {
+      case 'Price (low to high)':
+        setNfts(sortedNfts.sort((a, b) => a.price - b.price));
+        break;
+      case 'Price (high to low)':
+        setNfts(sortedNfts.sort((a, b) => b.price - a.price));
+        break;
+      case 'Recently added':
+        setNfts(sortedNfts.sort((a, b) => b.tokenId - a.tokenId));
+        break;
+      default:
+        setNfts(nfts);
+        break;
+    }
+  }, [activeSelect]);
+  const onHandleSearch = (value) => {
+    const filteredNfts = nfts.filter(({ name }) => name.toLowerCase().includes(value.toLowerCase()));
+
+    if (filteredNfts.length === 0) {
+      setNfts(nftsCopy);
+    } else {
+      setNfts(filteredNfts);
+    }
+  };
+
+  const onClearSearch = () => {
+    if (nfts.length && nftsCopy.length) {
+      setNfts(nftsCopy);
+    }
+  };
   if (isLoading) {
     return (
       <div className="flexStart min-h-screen">
